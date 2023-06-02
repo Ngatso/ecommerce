77a91b1914f3ba245db0f5126126f09cca5d8c01
kunsang <tenkus47@gmail.com>
2023-06-02 16:29:27 +0530

    fixed handle-supabase-auth

--- app/routes/handle-supabase-auth.tsx
+++ app/routes/handle-supabase-auth.tsx
@@ -1,7 +1,3 @@
 import { ActionArgs, ActionFunction } from "@remix-run/node";
 
-export const action: ActionFunction = async ({ request }: ActionArgs) => {
-  let formdata = await request.formData();
-  let user = formdata.get("user") as string;
-  return user;
-};
+export const action: ActionFunction = async ({ request }: ActionArgs) => {};
