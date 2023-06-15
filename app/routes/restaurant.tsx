a1e11699e5f71e3bda6666d2e9e23af06fb51c36
kunsang <tenkus47@gmail.com>
2023-06-15 23:33:12 +0530

    capitalise name

--- app/routes/restaurant.tsx
+++ app/routes/restaurant.tsx
@@ -31,13 +31,13 @@ function Restaurant({ restaurant }:{restaurant:restaurantType}) {
     <div className="max-w-xs flex flex-col justify-center">
       <div className="max-h-[245px] max-w-[360px] overflow-hidden">
         <img
-          className="w-full object-cover"
+          className="w-full object-cover hover:scale-105 transition-all duration-500 ease-in-out rounded" 
           src={
             "https://images.squarespace-cdn.com/content/v1/60de2756bdea384623d3b191/1682875483861-B6G9PTY2CMDOVPITG8BB/20230120_190852.jpg?format=500w"
           }
         />
       </div>
-      <div className="text-lg">{name}</div>
+      <div className="text-lg text-center py-2 capitalize">{name}</div>
       <button
         type="button"
         className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
